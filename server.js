2ea08fb3891800cda9b3ed2158572f5ffcf959fb
devinbradfield <devinbradfield@gmail.com>
2022-03-04 08:24:40 -0700

    finised server task

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const http = require("http");
+const http = require('http');
 
-// YOUR NODE SERVER CODE GOES HERE
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Galvanize');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at ${hostname}:${port}/`);
+});
